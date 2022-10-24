3c57c6c6ef711b624d8535e50cc6bd35fad834a8
natehalsey <halseyng@outlook.com>
2022-10-24 11:48:34 -0400

    Add readme, get app running on uvicorn

--- fridget-app/fridget.py
+++ fridget-app/fridget.py
@@ -1,7 +1,10 @@
 
 from fastapi import FastAPI
 from .ingredients.views import router as IngredientRouter
+from .recipes.views import router as RecipeRouter
+
 
 app = FastAPI()
 
 app.include_router(IngredientRouter)
+app.include_router(RecipeRouter)
