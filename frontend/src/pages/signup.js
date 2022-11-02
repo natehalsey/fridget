828ce02c82e8016bce5e2395979d26b6f2234407
Adam Salaymeh <asalaymeh@gmail.com>
2022-11-01 20:46:02 -0400

    Google sign in button added.

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 import styles from "./pages.css";
+import { useLocation } from "react-router-dom";
+
+
 const SignUp = () => {
+  const location = useLocation();
+  const { state } = location;
+  console.log(state.props);
+
   return (
     <div className={styles.staticpage}>
       <h1>SignUp</h1>
     </div>
   );
 };
-  
+
 export default SignUp;
