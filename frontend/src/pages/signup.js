54072e348f9942950c3c095950489a6ac25a6776
Adam Salaymeh <asalaymeh@gmail.com>
2022-11-01 20:56:21 -0400

    Added basic sign out functionality

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,13 +1,7 @@
 import React from "react";
 import styles from "./pages.css";
-import { useLocation } from "react-router-dom";
-
 
 const SignUp = () => {
-  const location = useLocation();
-  const { state } = location;
-  console.log(state.props);
-
   return (
     <div className={styles.staticpage}>
       <h1>SignUp</h1>
