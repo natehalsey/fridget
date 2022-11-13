6eea4fbeeac4e7987848ecc1ecaea2a8918ca975
Nate <halseyng@outlook.com>
2022-11-13 18:18:39 -0500

    remove areas path

--- backend/fridget/areas/views.py
+++ backend/fridget/areas/views.py
@@ -6,11 +6,6 @@ from fridget.base.schema import Area
 router = APIRouter(
     prefix = "/areas"
 )
-
-@router.get("/get-areas")
-async def get_areas():
-    return await Area.objects.all()
-
 @router.get("/get-recipes-by-area")
 async def get_recipes_by_area(area_list_model: AreaListModel):
 
