bd56d6800d65af75beda91ed35ee0f1219305be6
Ben Morgenstern <ben.morgenstern8@gmail.com>
2022-11-14 16:58:13 -0500

    Main FE UI updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppContext } from './AppContext';
 import Home from './pages/Home';
 import About from './pages/About';
 import Teams from './pages/Team';
-import SignUp from './pages/SignUp';
-  
+import SignUp from './pages/Signup';
+
+
 function App() {
+  const [userinfo, setUserInfo] = React.useState();
+  const [searchParams, setSearchParams] = React.useState();
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/Home' element={<Home />} />
-        <Route path='About' element={<About />} />
-        <Route path='/Team' element={<Teams />} />
-        <Route path='/Signup' element={<SignUp />} />
-        <Route path='/Api' element={<Link to={{ pathname: 'https://www.themealdb.com/'}}/> }/>
-      </Routes>
-    </Router>
+    <AppContext.Provider value={{ userinfo, setUserInfo, searchParams, setSearchParams}}>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/team' element={<Teams />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/api' element={<Link to={{ pathname: 'https://www.themealdb.com/'}}/> }/>
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
 }
   
