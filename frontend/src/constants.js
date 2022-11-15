a87fb00808c83592926987abe459dc4efaf9353d
Ben Morgenstern <ben.morgenstern8@gmail.com>
2022-11-15 15:49:09 -0500

    UI updates

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,19 @@
+import { createContext } from 'react'
+
+export const AppContext = createContext({})
 export const baseURL = "http://localhost:8000";
 export const getRecipeByNameURL = "/recipes/get-recipes-by-name";
 export const getRecipeByAreaURL = "/areas/get-recipes-by-area";
 export const getRecipeByCategoryURL = "/categories/get-recipes-by-category";
 export const getRecipeByIngredientURL = "/ingredients/get-recipes-by-ingredients";
 export const getRecipeByIdURL = "/recipes/get-recipes-by-id";
+export const searchParamName = "name";
+export const searchParamArea = "area";
+export const searchParamCategory = "category";
+export const searchParamIngredient = "ingredients";
+export const endpointMap = new Map([
+    [searchParamName, baseURL + getRecipeByNameURL],
+    [searchParamArea, baseURL + getRecipeByAreaURL],
+    [searchParamCategory, baseURL + getRecipeByCategoryURL],
+    [searchParamIngredient, baseURL + getRecipeByIngredientURL]
+  ]);
