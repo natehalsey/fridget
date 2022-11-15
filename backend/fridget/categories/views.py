d98acf2840621cc0a23b125a2b7cdf115a561406
natehalsey <natehalsey>
2022-11-15 14:06:39 -0500

    update views

--- backend/fridget/categories/views.py
+++ backend/fridget/categories/views.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fridget.recipes.models import RecipeModel, CategoryRecipesModel
 
 from fridget.base.schema import Category
 
@@ -8,8 +9,9 @@ router = APIRouter(
 )
 
 @router.get("/get-recipes-by-category")
-async def get_recipes_by_category(category: str):
-    return await Category.objects.select_related("recipes").filter(
+async def get_recipes_by_category(category: str) -> list[RecipeModel]:
+    categories: list[CategoryRecipesModel] = await Category.objects.select_related("recipes").filter(
         name__contains=category
     ).all()
 
+    return [recipe for category in categories for recipe in category.recipes]
