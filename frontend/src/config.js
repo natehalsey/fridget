0640dc0937deba1da27a7554267ea1f5f20e9980
natehalsey <natehalsey>
2022-11-26 15:28:41 -0500

    -

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const defaultSettings = {
+const settings = {
   REACT_APP_API_URL: 'http://localhost:8000',
+  ...process.env
 };
-const settings = {defaultSettings, ...process.env};
 export default settings;
