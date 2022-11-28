08ecc7b3a25b4142aac08e250f6f6bc889483b3f
Ishaan Kumar <ikumar4@uwo.ca>
2022-11-28 17:05:51 -0500

    Recipe Create component

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from "./Pages.css";
+import RecipeCreate from '../components/RecipeCreate';
+//import styles from "./Pages.css";
 const About = () => {
   return (
-    <div className={styles.staticpage}>
-      <h1>About</h1>
-    </div>
+    <RecipeCreate />
   );
 };
 export default About;
