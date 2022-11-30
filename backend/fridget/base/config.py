1a76bf8d043b5ec5c7e61aad3d3b69079f46b814
Ben Morgenstern <ben.morgenstern8@gmail.com>
2022-11-30 14:03:08 -0500

    changes for docker compose with db

--- backend/fridget/base/config.py
+++ backend/fridget/base/config.py
@@ -1,7 +1,14 @@
+import os
+
+################################
+#os.environ["DATABASE_URL"] = "postgresql://dev:fridget@172.17.0.2:5432/postgres" #postgresql://postgres:SRql605syVtnpUtP@74.119.150.172:5432/fridget"
+#os.environ["SECRET_KEY"] = "8707c3c1c1938e0934c67a8e54dddadd6be0621aaba8dcc8cc90d7aaecd9ab3c"
+################################
+
 class Settings:
-    DATABASE_URL = "postgresql://postgres:SRql605syVtnpUtP@74.119.150.172:5432/fridget" # env var
-    SECRET_KEY = "8707c3c1c1938e0934c67a8e54dddadd6be0621aaba8dcc8cc90d7aaecd9ab3c" # change to env var
-    ALGORITHM = "HS256" 
+    DATABASE_URL = os.environ.get("DATABASE_URL")# env var
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
 settings = Settings()
---
+++ prod_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export DATABASE_URL="postgresql://postgres:SRql605syVtnpUtP@74.119.150.172:5432/fridget"
+export REACT_APP_API_URL="http://fridget.co:81"
+export SECRET_KEY="8707c3c1c1938e0934c67a8e54dddadd6be0621aaba8dcc8cc90d7aaecd9ab3c"
